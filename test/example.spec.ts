d83b93b55ffa4deee096535719651f9bc0de2a35
ro7an22 <87031341+rohan-ailani@users.noreply.github.com>
2026-05-05 16:01:18 -0400

    initializing repository

---
+++ test/example.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://www.google.com');
+  await expect(page).toHaveTitle(/Google/);
+});
